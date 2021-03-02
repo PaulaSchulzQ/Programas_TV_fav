fdde4e866c79f2ff9b9f5c7ac769aec1401d4a7d
Your Name <paula.schulz.01@gmail.com>
2021-03-02 16:07:54 -0300

    requirements

--- show_app/views.py
+++ show_app/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 
-# Create your views here.
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Show
+from django.contrib import messages
 from time import gmtime, strftime
 
 def index(request):
@@ -18,9 +17,17 @@ def newshows(request):
     return render(request, "new_shows.html")
 
 def submitshow(request):
-    if request.method == "POST":
-        show_ob = Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], desc=request.POST['desc'])
-        return redirect(f'/shows/{show_ob.id}')
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        # si el diccionario de errores contiene algo, recorra cada par clave-valor y cree un mensaje flash
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirigir al usuario al formulario para corregir los errores
+        return redirect(f'/shows/new')
+    else:
+        if request.method == "POST":
+            show_ob = Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], desc=request.POST['desc'])
+            return redirect(f'/shows/{show_ob.id}')
 
 def show(request, show_id):
     context = {
@@ -41,13 +48,20 @@ def edit(request, show_id):
     return render(request, "edit.html", context)
 
 def submitedit(request, show_id):
-    editshow = Show.objects.get(id=show_id)
-    editshow.title = request.POST['title']
-    editshow.network = request.POST['network']
-    editshow.release_date = request.POST['release_date']
-    editshow.desc = request.POST['desc']
-    editshow.save()
-    return redirect(f'/shows/{editshow.id}')
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{show_id}/edit')
+    else:
+        editshow = Show.objects.get(id=show_id)
+        editshow.title = request.POST['title']
+        editshow.network = request.POST['network']
+        editshow.release_date = request.POST['release_date']
+        editshow.desc = request.POST['desc']
+        editshow.save()
+        messages.success(request, "Blog successfully updated")
+        return redirect(f'/shows/{editshow.id}')
 
 
 def delete(request, show_id):
